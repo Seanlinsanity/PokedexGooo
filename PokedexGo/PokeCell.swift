27df48ba7c008799e360b42a177529c38fe53b35
SEAN <SEAN@SEANMacBookAir.local>
2017-08-11 15:59:14 +0800

    collectionView function

---
+++ PokedexGo/PokeCell.swift
@@ -0,0 +1,35 @@
+//
+//  PokeCell.swift
+//  PokedexGo
+//
+//  Created by SEAN on 2017/8/11.
+//  Copyright © 2017年 SEAN. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+        //do the rounded corner
+    }
+    
+    
+    func configureCell(pokemon: Pokemon) {
+        
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    
+    }
+    
+}
